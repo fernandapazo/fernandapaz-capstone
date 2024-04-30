636cc99f75a4d88497802f9ad201c76c945ada1b
Fernanda <pazocampo.fer@gmail.com>
2024-04-30 11:01:57 -0700

    Creating BirdFinder Header, with logo and nav bar, some styling added.

--- birdfinder-app/src/components/Header/Header.jsx
+++ birdfinder-app/src/components/Header/Header.jsx
@@ -1,25 +1,35 @@
 import React from "react";
+import BirdFinderLogo from "../../assets/BirdFinder-logo.png";
+import userPic from "../../assets/use ph image copy.jpg";
+import "./Header.scss";
 
 function Header() {
   return (
-    <header>
-      <div className="logo">BirdFinder</div>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Search</a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="fas fa-user"></i>
-            </a>
-          </li>
-          {/* Replace the <i> tag with your user profile icon */}
-        </ul>
-      </nav>
+    <header className="header">
+      <div className="header__container">
+        <img className="header__logo-img" src={BirdFinderLogo} />
+        <nav className="header__nav">
+          <ul className="header__nav-list">
+            <li className="header__nav-item">
+              <a className="header__nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="header__nav-item">
+              <a className="header__nav-link" href="#">
+                Search
+              </a>
+            </li>
+            <div className="header__nav-item-wrapper">
+              <li className="header__nav-item">
+                <a href="#">
+                  <img className="header__user-icon" src={userPic} />
+                </a>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
