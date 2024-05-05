9c55a96039d881d46347aea4cd561b6e8478bf83
Fernanda <pazocampo.fer@gmail.com>
2024-05-05 16:25:57 -0700

    Removing comments from BirdList

--- birdfinder-app/src/components/BirdList/BirdList.jsx
+++ birdfinder-app/src/components/BirdList/BirdList.jsx
@@ -8,19 +8,6 @@ const BirdList = ({ searchResults }) => (
         <Link to={`/bird/${bird.id}`} className="bird-link">
           <div className="bird-info">
             <h2 className="bird-info__name">{bird.name}</h2>
-            {/* <p className="bird-info__sci-name">
-              Scientific Name: {bird.sciName}
-            </p>
-            <p className="bird-info__status">Status: {bird.status}</p>
-            <p className="bird-info__region">
-              Region:
-              {bird.region.map((region, index) => (
-                <span key={index} className="bird-info__region-item">
-                  {index > 0 ? ", " : ""}
-                  {region}
-                </span>
-              ))}
-            </p> */}
             <div className="bird-info__images">
               {bird.images.map((image, index) => (
                 <img
