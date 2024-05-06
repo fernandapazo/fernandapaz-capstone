5047894c32831870e2167d8ba263730c0ba138c0
Fernanda <pazocampo.fer@gmail.com>
2024-05-05 18:38:18 -0700

    Creating Route for /Profile

--- birdfinder-app/src/App.jsx
+++ birdfinder-app/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import Footer from "./components/Footer/Footer";
 import BirdDetailsPage from "./pages/BirdDetailsPage/BirdDetailsPage";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/bird/:id" element={<BirdDetailsPage />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
         {/* <Footer /> */}
       </BrowserRouter>
