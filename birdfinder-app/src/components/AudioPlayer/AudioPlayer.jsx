99f19354d2872dc0b96aa5ef03acec36a834a49f
Fernanda <pazocampo.fer@gmail.com>
2024-05-06 15:20:36 -0700

    formatting date on recording date in audioplayer, and styling

--- birdfinder-app/src/components/AudioPlayer/AudioPlayer.jsx
+++ birdfinder-app/src/components/AudioPlayer/AudioPlayer.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "./AudioPlayer.scss";
 
 function AudioPlayer() {
   const [recordings, setRecordings] = useState([]);
@@ -22,17 +23,48 @@ function AudioPlayer() {
       });
   }, []);
 
+  const formatDate = (dateString) => {
+    const dateParts = dateString.split("-");
+    const year = dateParts[0];
+    const month = dateParts[1];
+    const day = dateParts[2];
+
+    const date = new Date(year, month - 1, day);
+
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const monthName = monthNames[date.getMonth()];
+
+    return `${monthName} ${day}, ${year}`;
+  };
+
   return (
-    <div>
+    <div className="recordings">
       {recordings.map((recording, index) => (
-        <div key={index}>
-          <audio controls>
+        <div className="recording" key={index}>
+          <audio className="recording-audio" controls>
             <source src={recording.file} type="audio/mpeg" />
           </audio>
-          <p>
-            Recording {index + 1}: {recording.type} - {recording.date},
-            Location: {recording.loc}
-          </p>
+          <div className="recording-info">
+            <span className="recording-info-date">
+              Recording {index + 1}: {formatDate(recording.date)}
+            </span>
+            <span className="recording-info-loc">
+              Location: {recording.loc}{" "}
+            </span>
+          </div>
         </div>
       ))}
     </div>
@@ -40,3 +72,5 @@ function AudioPlayer() {
 }
 
 export default AudioPlayer;
+// {recording.type} -
+// Recording {index + 1}: {recording.date}{" "}
