e5facf0e95dbd16fb81d6b9eaca1539830ed38b8
Fernanda <pazocampo.fer@gmail.com>
2024-05-06 20:33:48 -0700

    Minor changes to jsx file, adding styling

--- birdfinder-app/src/components/BirdList/BirdList.jsx
+++ birdfinder-app/src/components/BirdList/BirdList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./BirdList.scss";
 
 const BirdList = ({ searchResults }) => (
   <ul className="bird-list">
@@ -8,16 +9,18 @@ const BirdList = ({ searchResults }) => (
         <Link to={`/bird/${bird.id}`} className="bird-link">
           <div className="bird-info">
             <h2 className="bird-info__name">{bird.name}</h2>
-            <div className="bird-info__images">
-              {bird.images.map((image, index) => (
-                <img
-                  key={index}
-                  src={image}
-                  alt={`Bird ${index + 1}`}
-                  className="bird-info__image"
-                />
-              ))}
-            </div>
+            {bird.images.length > 0 && (
+              <div className="bird-info__hover-images">
+                {bird.images.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`Bird ${index + 1}`}
+                    className="bird-info__image"
+                  />
+                ))}
+              </div>
+            )}
           </div>
         </Link>
       </li>
