36af13a09dc4d121eef406ab414ffb205b246437
Fernanda <pazocampo.fer@gmail.com>
2024-05-06 20:34:37 -0700

    Adding classnames and styling

--- birdfinder-app/src/components/FavoritesList/FavoritesList.jsx
+++ birdfinder-app/src/components/FavoritesList/FavoritesList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./FavoritesList.scss";
 
 function FavoritesList() {
   const [favorites, setFavorites] = useState([]);
@@ -13,12 +14,15 @@ function FavoritesList() {
   }, []);
 
   return (
-    <div>
-      <h2>Favorites List</h2>
-      <ul>
+    <div className="favorites-list">
+      <ul className="favorites-list__items">
         {favorites.map((favorite, index) => (
-          <li key={index}>
-            <Link to={`/bird/${favorite.id}`}>{favorite.name}</Link>
+          <li key={index} className="favorites-list__item">
+            <Link to={`/bird/${favorite.id}`} className="favorites-list__link">
+              <div className="favorites-list__info">
+                <span className="favorites-list__name">{favorite.name}</span>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
